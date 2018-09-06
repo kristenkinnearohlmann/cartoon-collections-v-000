7ba639675258e46ffe9193623de8d9b861656622
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-06 20:26:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,13 @@ def summon_captain_planet(veggies)# code an argument here
   veggies.collect { |veggie| "#{veggie.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  if calls.length.any? > 4
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
