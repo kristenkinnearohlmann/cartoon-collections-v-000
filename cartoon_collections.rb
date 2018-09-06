5168a684084d4e3a2efc8e953576b9973e628d0a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-06 20:23:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,7 @@ end
 
 def summon_captain_planet(veggies)# code an argument here
   # Your code here
-  binding.pry
   veggies.collect { |veggie| "#{veggie.capitalize}!"}
-  veggies
 end
 
 def long_planeteer_calls# code an argument here
